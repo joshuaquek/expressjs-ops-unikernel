0e68a5ca69baa316134b9a695cb3b09bb92ba970
Joshua Quek <joshua.quek.fullstack@gmail.com>
2021-10-21 15:18:40 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// ---- NPM Imports ----
+require('dotenv').config()
+const bodyParser = require('body-parser');
+const express = require('express');
+const logger = require('morgan');
+
+// ---- Express Server Setup ----
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'));
+
+// ---- Express Server Endpoints ----
+app.get('/', (req, res) => {
+  res.json({ status: 'Server is running.' } );
+});
+
+console.log('Server running at http://127.0.0.1:80/')
+app.listen(process.env.PORT || 80); // Create a .env to set env variables
